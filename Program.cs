c1e9617540cfc248fa36843f60e4d9bb81365cc0
Tali Avni <tali@aristo7.com>
2020-08-06 14:02:38 -0700

    writing point, starting on logic

--- Program.cs
+++ Program.cs
@@ -8,6 +8,18 @@ namespace snake_game
         int Height = 20;
         int Width = 30;
 
+        int[] X = new int[50];
+        int[] Y = new int[50];
+
+        int pointX;
+        int pointY;
+
+        int parts = 3;
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        char key = 'W';
+
+        Random rnd = new Random();
+
         public void WriteBoard()
         {
            Console.Clear();
@@ -36,10 +48,64 @@ namespace snake_game
 
            }
         }
+
+        public void Input()
+        {
+            if(Console.KeyAvailable)
+            {
+                Console.ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
+        }
+        public void WritePoint(int x, int y)
+        {
+            Console.SetCursorPosition(x,y);
+            Console.Write("#");
+        }
+
+        public void Logic()
+        {
+            if(X[0]==pointX)
+            {
+                if(Y[0]==pointY)
+                {
+                    parts++;
+                    pointX = rnd.Next(2, (Width - 2));
+                    pointY = rnd.Next(2, (Height - 2));
+
+                }
+            }
+            for(int i = parts; i > 1; i--)
+            {
+                X[i - 1] = X [i - 2];
+                Y[i - 1] = Y [i - 2];
+            }
+
+            switch(key)
+            {
+                case 'w':
+                Y[0]--;
+                break;
+
+                case 's':
+                Y[0]++;
+                break;
+
+                case 'a':
+                X[0]--;
+                break;
+
+                case 'd':
+                X[0]++;
+                break;
+            }
+        }
         static void Main(string[] args)
         {
             Snake snake = new Snake();
             snake.WriteBoard();
+            snake.Input();
+            snake.WritePoint(1, 1);
             Console.ReadKey();
         }
     }
